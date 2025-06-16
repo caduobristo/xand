4fa55ac7a96d98922feaac22f236e62c01b1bd17
Carlos Eduardo Obristo <carlosobristo@alunos.utfpr.edu.br>
2025-06-16 19:35:40 -0300

    Sprites minigame

--- xand_app/lib/minigame/components/flying_xand.dart
+++ xand_app/lib/minigame/components/flying_xand.dart
@@ -9,13 +9,19 @@ class FlyingXand extends SpriteComponent
   final double _jumpForce = 350;
   Vector2 _velocity = Vector2.zero();
 
+  late Sprite _spriteParado;
+  late Sprite _spriteVoando;
+
   FlyingXand();
 
   @override
   Future<void> onLoad() async {
-    sprite = await gameRef.loadSprite('respirando.png');
+    _spriteParado = await gameRef.loadSprite('astronauta1.png');
+    _spriteVoando = await gameRef.loadSprite('astronauta2.png');
+
+    sprite = _spriteParado;
     position = Vector2(50, gameRef.size.y / 2 - size.y / 2);
-    size = Vector2.all(50);
+    size = Vector2.all(60);
     add(CircleHitbox());
   }
 
@@ -33,13 +39,22 @@ class FlyingXand extends SpriteComponent
 
   void fly() {
     _velocity.y = -_jumpForce;
-    // Efeito de rotação ao pular
     add(
       RotateEffect.to(
-        -0.2, // Rotação para cima em radianos
+        -0.2,
         EffectController(duration: 0.1, reverseDuration: 0.3),
       ),
     );
+    sprite = _spriteVoando;
+
+    children.whereType<TimerComponent>().forEach((timer) => timer.removeFromParent());
+    add(TimerComponent(
+      period: 0.3,
+      removeOnFinish: true,
+      onTick: () {
+        sprite = _spriteParado;
+      },
+    ));
   }
 
   @override
